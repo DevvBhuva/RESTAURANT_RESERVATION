6008365e4e2749782734df15d0469adcd3a18e18
DevvBhuva <devbhuva07@gmail.com>
2024-03-31 09:49:25 +0530

    Initial commit

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">Restuarent</div>
+          <div className="right">
+            <p></p>
+            <p></p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By Dev</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By Dev.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
